d0b82b187af3b6e3977ba396d5a98540ee26c2aa
Wiktor Wilczewski <wiktorczewski@gmail.com>
2026-04-27 22:17:16 +0200

    feat: add block memory gen as frame buffer, convert img_gen to memory tester, connect all changes in paint_top

--- mini-paint/mini-paint.srcs/sources_1/imports/ucisw2-projekt/img_gen.vhd
+++ mini-paint/mini-paint.srcs/sources_1/imports/ucisw2-projekt/img_gen.vhd
@@ -1,7 +1,6 @@
 -----------------------------------------------------------------------------
 --
---   Simple video image generator: animated square on some color background
---   J.Sugier, PWr Edu PL
+--   Framebuffer initializer (one-shot memory tester)
 --
 -----------------------------------------------------------------------------
 
@@ -14,106 +13,56 @@ entity img_gen is
     Port (
            Clk  : in STD_LOGIC;
            RstN : in STD_LOGIC;
-           PosX : in  STD_LOGIC_VECTOR (9 downto 0);
-           PosY : in  STD_LOGIC_VECTOR (9 downto 0);
-           R    : out STD_LOGIC_VECTOR (7 downto 0);
-           G    : out STD_LOGIC_VECTOR (7 downto 0);
-           B    : out STD_LOGIC_VECTOR (7 downto 0)
+           We   : out STD_LOGIC;
+           Addr : out STD_LOGIC_VECTOR (18 downto 0);
+           Data : out STD_LOGIC_VECTOR (7 downto 0)
 		);
 end img_gen;
 
 
 architecture Behavioral of img_gen is
 
-    -- Constants
     constant MODE_WIDTH  : integer := 640;
     constant MODE_HEIGHT : integer := 480;
 
-    -- Animated white square moves every frame
-    constant SQ_SIZE : integer := MODE_HEIGHT / 10;
-    -- position of left / top corner:
-    signal Sq_posX : signed (10 downto 0) := (others => '0');
-    signal Sq_posY : signed (10 downto 0) := (others => '0');
-    -- anim. speed; 1 px per frame
-    signal Sq_dX : signed (1 downto 0) := to_signed( 1,  2 );
-    signal Sq_dY : signed (1 downto 0) := to_signed( 1,  2 );
-    -- aux signals for out-of-range detection
-    signal next_posX, next_posY : signed (10 downto 0);
-    
-    -- PosX, PosY: conversion of std_logic_vector inputs to unsigned
-    signal sPosX, sPosY : signed(10 downto 0);
+    signal x_cnt : unsigned(9 downto 0) := (others => '0');
+    signal y_cnt : unsigned(9 downto 0) := (others => '0');
+    signal done  : std_logic := '0';
+
+    signal addr_calc : unsigned(18 downto 0);
+    signal r3 : std_logic_vector(2 downto 0);
+    signal g3 : std_logic_vector(2 downto 0);
+    signal b2 : std_logic_vector(1 downto 0);
 
 begin
-    sPosX <= signed( '0' & posX );
-    sPosY <= signed( '0' & posY );
 
-    -- RGB outputs (pixel color from PosX/PosY and square position)
-    process( sPosX, sPosY, Sq_posX, Sq_posY, PosX )
-    begin
-        if sPosX >= Sq_posX and sPosX < Sq_posX + SQ_SIZE and
-           sPosY >= Sq_posY and sPosY < Sq_posY + SQ_SIZE     then
-            -- inside white square
-            R <= (others => '1');
-            G <= (others => '1');
-            B <= (others => '1');
-        else
-            -- some color background
-            if sPosY < MODE_HEIGHT / 3 then
-                -- first 1/3 of height: red gradient
-                R <= PosX( 7 downto 0 );
-                G <= (others => '0');
-                B <= (others => '0');
-            elsif sPosY < MODE_HEIGHT * 2 / 3 then
-                -- second 1/3 of height: green gradient, inversed
-                R <= (others => '0');
-                G <= not PosX( 7 downto 0 );
-                B <= (others => '0');
-            else
-                -- third 1/3 of height: blue gradient
-                R <= (others => '0');
-                G <= (others => '0');
-                B <= PosX( 7 downto 0 );
-            end if;
-        end if;
-    end process;
+    addr_calc <= resize(y_cnt * MODE_WIDTH + x_cnt, 19);
+    Addr <= std_logic_vector(addr_calc);
 
-    -- Animation of the square
-    next_posX <= Sq_posX + Sq_dX;
-    next_posY <= Sq_posY + Sq_dY;
-    process( Clk )
+    r3 <= std_logic_vector(x_cnt(9 downto 7));
+    g3 <= std_logic_vector(x_cnt(6 downto 4));
+    b2 <= std_logic_vector(x_cnt(3 downto 2));
+    Data <= r3 & g3 & b2;
+
+    We <= not done;
+
+    process(Clk)
     begin
-        if rising_edge( Clk ) then
+        if rising_edge(Clk) then
             if RstN = '0' then
-                Sq_posX <= (others => '0');
-                Sq_posY <= (others => '0');
-                Sq_dX <= to_signed( 1,  2 );
-                Sq_dY <= to_signed( 1,  2 );
-            elsif sPosX = MODE_WIDTH - 1  and  sPosY = MODE_HEIGHT - 1 then     -- once per frame
-                -- update positionX
-                if next_posX < 0 then
-                    -- bounce from left
-                    Sq_posX <= ( others => '0' );
-                    Sq_dX <= to_signed( 1,  2 );
-                elsif next_posX > MODE_WIDTH - SQ_SIZE then
-                    -- bounce from right
-                    Sq_posX <= to_signed( MODE_WIDTH - SQ_SIZE, 11 );
-                    Sq_dX <= to_signed( -1,  2 );
+                x_cnt <= (others => '0');
+                y_cnt <= (others => '0');
+                done <= '0';
+            elsif done = '0' then
+                if x_cnt = to_unsigned(MODE_WIDTH - 1, x_cnt'length) then
+                    x_cnt <= (others => '0');
+                    if y_cnt = to_unsigned(MODE_HEIGHT - 1, y_cnt'length) then
+                        done <= '1';
+                    else
+                        y_cnt <= y_cnt + 1;
+                    end if;
                 else
-                    -- normal move
-                    Sq_posX <= next_posX;
-                end if;
-                -- update position Y
-                if next_posY < 0 then
-                    -- bounce from top
-                    Sq_posY <= ( others => '0' );
-                    Sq_dY <= to_signed( 1,  2 );
-                elsif next_posY > MODE_HEIGHT - SQ_SIZE then
-                    -- bounce from bottom
-                    Sq_posY <= to_signed( MODE_HEIGHT - SQ_SIZE, 11 );
-                    Sq_dY <= to_signed( -1,  2 );
-                else
-                    -- normal move
-                    Sq_posY <= next_posY;
+                    x_cnt <= x_cnt + 1;
                 end if;
             end if;
         end if;
--- mini-paint/mini-paint.srcs/sources_1/imports/ucisw2-projekt/paint_top.vhd
+++ mini-paint/mini-paint.srcs/sources_1/imports/ucisw2-projekt/paint_top.vhd
@@ -29,6 +29,23 @@ architecture structural of paint_top is
             clk_in1_n : in STD_LOGIC
         );
     end component;
+    
+    COMPONENT blk_mem_gen_0
+      PORT (
+        clka : IN STD_LOGIC;
+        ena : IN STD_LOGIC;
+        wea : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
+        addra : IN STD_LOGIC_VECTOR(18 DOWNTO 0);
+        dina : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        douta : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        clkb : IN STD_LOGIC;
+        enb : IN STD_LOGIC;
+        web : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
+        addrb : IN STD_LOGIC_VECTOR(18 DOWNTO 0);
+        dinb : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        doutb : OUT STD_LOGIC_VECTOR(7 DOWNTO 0) 
+      );
+    END COMPONENT;
 
     signal s_clk_25mhz   : STD_LOGIC;
     signal s_clk_125mhz  : STD_LOGIC;
@@ -38,6 +55,12 @@ architecture structural of paint_top is
     signal s_video_de    : STD_LOGIC;
     signal s_video_hsync : STD_LOGIC;
     signal s_video_vsync : STD_LOGIC;
+    signal s_video_de_d1 : STD_LOGIC;
+    signal s_video_de_d2 : STD_LOGIC;
+    signal s_video_hsync_d1 : STD_LOGIC;
+    signal s_video_hsync_d2 : STD_LOGIC;
+    signal s_video_vsync_d1 : STD_LOGIC;
+    signal s_video_vsync_d2 : STD_LOGIC;
     signal s_pixel_x     : STD_LOGIC_VECTOR(9 downto 0);
     signal s_pixel_y     : STD_LOGIC_VECTOR(9 downto 0);
     
@@ -45,8 +68,32 @@ architecture structural of paint_top is
     signal s_color_g     : STD_LOGIC_VECTOR(7 downto 0);
     signal s_color_b     : STD_LOGIC_VECTOR(7 downto 0);
 
+
+    signal s_mouse_we    : STD_LOGIC;
+    signal s_write_addr  : STD_LOGIC_VECTOR(18 downto 0);
+    signal s_write_data  : STD_LOGIC_VECTOR(7 downto 0);
+    signal s_read_addr   : STD_LOGIC_VECTOR(18 downto 0);
+    signal s_read_data   : STD_LOGIC_VECTOR(7 downto 0);
+
+    signal s_fb_ena       : STD_LOGIC;
+    signal s_fb_enb       : STD_LOGIC;
+    signal s_fb_wea       : STD_LOGIC_VECTOR(0 downto 0);
+    signal s_fb_web       : STD_LOGIC_VECTOR(0 downto 0);
+    signal s_fb_dinb      : STD_LOGIC_VECTOR(7 downto 0);
 begin
     s_sys_reset_n <= s_pll_locked and not i_reset_n;
+
+    s_fb_ena <= '1';
+    s_fb_enb <= '1';
+    s_fb_wea(0) <= s_mouse_we;
+    s_fb_web <= (others => '0');
+    s_fb_dinb <= (others => '0');
+
+    s_read_addr <= std_logic_vector(resize(unsigned(s_pixel_y) * 640 + unsigned(s_pixel_x), 19));
+
+    s_color_r <= s_read_data(7 downto 5) & s_read_data(7 downto 5) & s_read_data(7 downto 6);
+    s_color_g <= s_read_data(4 downto 2) & s_read_data(4 downto 2) & s_read_data(4 downto 3);
+    s_color_b <= s_read_data(1 downto 0) & s_read_data(1 downto 0) & s_read_data(1 downto 0) & s_read_data(1 downto 0);
     
     u_clk_wiz_0: clk_wiz_0
         port map ( 
@@ -73,23 +120,42 @@ begin
 
     u_img_gen: entity work.img_gen
         port map (
-            Clk      => s_clk_25mhz,
-            RstN  => s_sys_reset_n,
-            PosX  => s_pixel_x,
-            PosY  => s_pixel_y,
-            R  => s_color_r,
-            G  => s_color_g,
-            B  => s_color_b
+            Clk  => s_clk_125mhz,
+            RstN => s_sys_reset_n,
+            We   => s_mouse_we,
+            Addr => s_write_addr,
+            Data => s_write_data
         );
+
+    process(s_clk_25mhz)
+    begin
+        if rising_edge(s_clk_25mhz) then
+            if s_sys_reset_n = '0' then
+                s_video_de_d1 <= '0';
+                s_video_de_d2 <= '0';
+                s_video_hsync_d1 <= '0';
+                s_video_hsync_d2 <= '0';
+                s_video_vsync_d1 <= '0';
+                s_video_vsync_d2 <= '0';
+            else
+                s_video_de_d1 <= s_video_de;
+                s_video_de_d2 <= s_video_de_d1;
+                s_video_hsync_d1 <= s_video_hsync;
+                s_video_hsync_d2 <= s_video_hsync_d1;
+                s_video_vsync_d1 <= s_video_vsync;
+                s_video_vsync_d2 <= s_video_vsync_d1;
+            end if;
+        end if;
+    end process;
 
     u_hdmi_tx: entity work.HDMI_TX_wrap
         port map (
             pxClk      => s_clk_25mhz,
             pxClkX5    => s_clk_125mhz,
             ResetN     => s_sys_reset_n,
-            DE         => s_video_de,
-            HSync      => s_video_hsync,
-            VSync      => s_video_vsync,
+            DE         => s_video_de_d2,
+            HSync      => s_video_hsync_d2,
+            VSync      => s_video_vsync_d2,
             R          => s_color_r,
             G          => s_color_g,
             B          => s_color_b,
@@ -102,5 +168,21 @@ begin
             HDMI_CK_P  => o_hdmi_clk_p,
             HDMI_CK_N  => o_hdmi_clk_n
         );
+        
+    u_framebuffer : blk_mem_gen_0
+      PORT MAP (
+                clka => s_clk_125mhz,
+                ena => s_fb_ena,
+                wea => s_fb_wea,
+                addra => s_write_addr,
+                dina => s_write_data,
+                douta => open,
+                clkb => s_clk_25mhz,
+                enb => s_fb_enb,
+                web => s_fb_web,
+                addrb => s_read_addr,
+                dinb => s_fb_dinb,
+                doutb => s_read_data
+      );
 
 end structural;
